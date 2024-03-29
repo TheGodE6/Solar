e263695d84d05c1d0fd33b07e46296dd97db00c1
TheGodE6 <110555637+TheGodE6@users.noreply.github.com>
2024-03-28 20:46:27 -0400

    Update Radiation Hub.lua

--- Radiation Hub.lua
+++ Radiation Hub.lua
@@ -100,7 +100,6 @@ game.Players.LocalPlayer.Chatted:Connect(function(msg)
                chat ("size me 0.3")
                chat ("size me 0.3")
                chat ("size me 0.3")
-               chat ("unsize me")
                chat ("blind all")
                chat ("h \n\n\nServer is now closed by Radiation Hub.\n\n\n")
         end

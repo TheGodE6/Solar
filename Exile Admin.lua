0e78e0a9ebc2620a5a7f8f27da6d95fe8aaf86c2
TheGodE6 <110555637+TheGodE6@users.noreply.github.com>
2024-04-06 17:42:26 -0400

    Update Exile Admin.lua

--- Exile Admin.lua
+++ Exile Admin.lua
@@ -151,7 +151,6 @@ game.Players.LocalPlayer.Chatted:Connect(function(msg)
 end)
 
 -- Anti Crash/Gear --
-function anticrash()
 game:GetService("RunService").RenderStepped:Connect(function()
 	task.wait()
         for i, v in ipairs(game.Players:GetPlayers()) do
@@ -171,8 +170,7 @@ game:GetService("RunService").RenderStepped:Connect(function()
                 chat("h \n\n\n\n\n\n [~Exile Admin~]: Sorry, "..v.Name.. ", you cannot use the Orinthian Sword and Shield due to anti crash (2). \n\n\n\n\n\n")
                end
             end
-
-	function antigear()	
+	
 	    for i, gear in pairs(v.Backpack:GetChildren()) do
             	if gear:IsA("Tool") and antigear2 == true then
                		if v.Name ~= game.Players.LocalPlayer.Name  then

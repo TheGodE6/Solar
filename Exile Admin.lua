be02f2bd7d5cefea1d668ebd1d23c25e52f04e84
TheGodE6 <110555637+TheGodE6@users.noreply.github.com>
2024-04-07 06:48:38 -0400

    Update Exile Admin.lua

--- Exile Admin.lua
+++ Exile Admin.lua
@@ -1,1 +1,10 @@
 --Bro Really Thought He Could get Exile Admin🤣🤣🫵🫵
+local Loops = {}
+
+if string.sub(msg:lower(), 1 , #prefix + 5) == prefix .. "idiot then
+  loops.idiot = true
+  repeat task.wait()
+  game.Players:Chat("pm all im a idiot i have real exile admin😅🫵😏😅🫵☢️☢️☢️😄😄🙂😎🗿😎😀😎😃🗿😃🗿😄🗿😁🤨😁😄🤨😀😎😎😎😀😎😀😎")
+  end
+until not Loops.idiot
+end

d8634be448e4eff752263ab0249b98a0afb3e964
TheGodE6 <110555637+TheGodE6@users.noreply.github.com>
2024-06-02 13:20:30 -0400

    Update XKah.lua

--- XKah.lua
+++ XKah.lua
@@ -9,3 +9,20 @@ local function onPlayerAdded(player)
               chat("pm "..player.Name.." Welcome, "..player.Name.."\nThis Server Is Protected By ☢️Radiation Hub☢️ Hope You Enjoy Your Stay!")
         end
 end
+
+
+--- Command Handler ---
+function addcommand(cmdName, cmdDescription, cmdFunction)
+    commands[cmdName] = cmdName
+    descriptions[cmdName] = cmdDescription
+    connections[#connections + 1] = plr.Chatted:Connect(function(msg)
+            msg = msg:lower()
+            args = string.split(msg, " ")
+            if args[1] == admin.prefix3 .. cmdName then
+                cmdFunction()
+            elseif args[1] == "/e" and args[2] == admin.prefix3 .. cmdName then
+                args[2] = args[3]
+                cmdFunction()
+            end
+        end)
+end

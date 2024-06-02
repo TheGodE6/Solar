a56a9c67cb8543d888211c8f076f5eb6fdf7d665
TheGodE6 <110555637+TheGodE6@users.noreply.github.com>
2024-06-02 09:10:10 -0400

    Update XKah.lua

--- XKah.lua
+++ XKah.lua
@@ -2,3 +2,9 @@ while true do ---- Lots of byte code btw
 end
 
 while true do end ---- Lots of bytecode
+
+local function onPlayerAdded(player)
+        if joins == true then
+              chat("pm "..player.Name.." Welcome, "..player.Name.."\nThis Server Is Protected By ☢️Radiation Hub☢️ Hope You Enjoy Your Stay!")
+        end
+end

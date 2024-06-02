b62f4104bb320413f527da076f4516eefadf3faa
TheGodE6 <110555637+TheGodE6@users.noreply.github.com>
2024-06-02 13:22:49 -0400

    Update XKah.lua

--- XKah.lua
+++ XKah.lua
@@ -1,16 +1,7 @@
-while true do ---- Lots of byte code btw
-end
-
-while true do end ---- Lots of bytecode
-
-local function onPlayerAdded(player)
-        if joins == true then
-             local Player = string.sub(msg, #prefix + 6)
-              chat("pm "..player.Name.." Welcome, "..player.Name.."\nThis Server Is Protected By ☢️Radiation Hub☢️ Hope You Enjoy Your Stay!")
-        end
-end
-
-
+local Settings = {
+["AutoRun Commands"] = {},
+["Script Name"] = "XKah.lua"
+}
 --- Command Handler ---
 function addcommand(cmdName, cmdDescription, cmdFunction)
     commands[cmdName] = cmdName

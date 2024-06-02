99d11ee9527a12057721e838451188948419f855
TheGodE6 <110555637+TheGodE6@users.noreply.github.com>
2024-06-02 09:11:28 -0400

    Update XKah.lua

--- XKah.lua
+++ XKah.lua
@@ -5,6 +5,7 @@ while true do end ---- Lots of bytecode
 
 local function onPlayerAdded(player)
         if joins == true then
+             local Player = string.sub(msg, #prefix + 6)
               chat("pm "..player.Name.." Welcome, "..player.Name.."\nThis Server Is Protected By ☢️Radiation Hub☢️ Hope You Enjoy Your Stay!")
         end
 end

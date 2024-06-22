70425a169e24de641d117d5511cac05580cd2a10
TheGodE6 <110555637+TheGodE6@users.noreply.github.com>
2024-06-22 14:58:39 -0400

    Update Rekkos-Hub!.lua

--- Rekkos-Hub!.lua
+++ Rekkos-Hub!.lua
@@ -1,2 +1,9 @@
- local Settings = {
-["prefix"] = "-"bbb
+local Settings = {
+["Version"] = "0.1",
+["Autorun Commands"] = {}, -- Commands that will autorun upon start up
+["Default Whitelisted"] = {}, -- People that are whitelisted by default
+["Prefix"] = ">", -- The prefix that you want to use for this script
+["Joins"] = false, ---- When a user joins, they get PMed about the fact this script protects the server
+["Autoafk"] = false, -- When you're AFK, it will name you AFK, god you and ff you. Still in development
+["Script Name"] = "["Rekkos-Hub-Beta"] -- Name of the script
+}

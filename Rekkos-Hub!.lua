0a73422d42279026620b49a63ed671b7ba2f72a5
S-PScripts <ts2021me@outlook.com>
2024-06-23 12:53:37 +0100

    Update Rekkos-Hub!.lua

--- Rekkos-Hub!.lua
+++ Rekkos-Hub!.lua
@@ -1,11 +1,13 @@
- local Settings = {
+-- dawninja STOP CHANGING THE NAME OF YOUR SCRIPT!!!!!!!!
+
+local Settings = {
 ["Version"] = "0.1",
 ["Autorun Commands"] = {}, -- Commands that will autorun upon start up
 ["Default Whitelisted"] = {}, -- People that are whitelisted by default
 ["Prefix"] = ">", -- The prefix that you want to use for this script
 ["Joins"] = false, ---- When a user joins, they get PMed about the fact this script protects the server
 ["Autoafk"] = false, -- When you're AFK, it will name you AFK, god you and ff you. Still in development
-["Script Name"] = "["Rekkos-Hub-Beta"] -- Name of the script
+["Script Name"] = ["Rekkos-Hub-Beta"] -- Name of the script
 }
 
 --- Rekkos General Locals! ---
@@ -19,7 +21,7 @@ game.Players:Chat(msg)
 end
 
 
---- Command Handler ---
+--- Command Handler (stolen from ii) ---
 function addCommand(name,args,func)
     table.insert(commandlist,{name,args,func})
 end
@@ -42,10 +44,10 @@ end)
 end
 
 --- Command Hub! ---
-addCommand("Whitelist",{},function()
+addCommand("whitelist",{},function()
 end
 
-addCommand("mute,{"player"},function(args, MuteReason)
+addCommand("mute",{player},function(args, MuteReason)
 for i,v in pairs(GetPlayers(args[1])) do
 Loops.mute = true
 repeat task.wait()

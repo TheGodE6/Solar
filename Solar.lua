4d6fac638f90cf3d33cf0f9ac034e797ec166367
TheGodE6 <110555637+TheGodE6@users.noreply.github.com>
2024-07-06 04:39:57 -0700

    Update Solar.lua

--- Solar.lua
+++ Solar.lua
@@ -6786,33 +6786,36 @@ end)
 
 addCommand("slock",{},function()
         lpc.HumanoidRootPart.CFrame = CFrame.new(-41, 4, -3)
-          for i = 1,100 do
+          for i = 1,99 do
+              chat("hat me 18101265026")
+   for i = 1,99 do
               chat("hat me 18101265026")
           end
- for i = 1,100 do
+   for i = 1,99 do
               chat("hat me 18101265026")
           end
- for i = 1,100 do
+   for i = 1,99 do
               chat("hat me 18101265026")
           end
- for i = 1,100 do
+   for i = 1,99 do
               chat("hat me 18101265026")
           end
- for i = 1,100 do
+   for i = 1,99 do
               chat("hat me 18101265026")
           end
- for i = 1,100 do
+   for i = 1,99 do
               chat("hat me 18101265026")
           end
- for i = 1,100 do
+   for i = 1,99 do
               chat("hat me 18101265026")
           end
- for i = 1,100 do
+   for i = 1,99 do
               chat("hat me 18101265026")
           end
- for i = 1,100 do
+   for i = 1,99 do
               chat("hat me 18101265026")
-end
+          end
+
  runCommand(prefix.."unantiabuse")
 chat("respawn all")
           chat("size me 5")

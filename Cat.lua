1b80d6163673bfad0a0381db552ed392284d3721
TheGodE6 <110555637+TheGodE6@users.noreply.github.com>
2024-08-04 15:29:36 -0400

    Update Cat.lua

--- Cat.lua
+++ Cat.lua
@@ -15,8 +15,16 @@ end
 local tick = os.clock()
 local hideid = "00000000000000000000000000000"
 --//Tables
+local onstart = { -- Commands that get said when script loaded
+    "nok",
+    "antikill", 
+    "antihat",
+    "antipunish",
+    "antirocket",
+    "alogs"
+}
+
 local hbanned = {
-"atprog"
 }
 
 local rbanned = {
@@ -87,9 +95,6 @@ local Loops = {}
 local MarketplaceService = game:GetService("MarketplaceService")
 local UserInputService = game:GetService("UserInputService")
 local Players = game:GetService("Players")
-
-
-
 --//Functions
 local function antiSpam()
     local charset = {"£", "$", "%", "^", "&", "*", "-", "_", "+", "=", "!", "@", ";", "~","(", ")", "`", "\\", "|", "[", "]", "{", "}", ":", "'", ".", ",", "<", ">","||||||||||||||||||||||||"}
@@ -197,6 +202,12 @@ end
         end
     end
 end)
+
+local function alogs()
+for i  = 1,100 do
+chatshit("ff @#!#,###,##!@#$#!@#-!##!##!##")
+end
+end
 
 function hat(PlayerName,Nameballs)
     pcall(function()
@@ -947,36 +958,27 @@ chatshit("clone all all all")
 end
 end)
 
+addcmd({"alogs"},function()
+alogs()
+end)
+
 addcmd({"spam"}, function(args)
     local text = args[2]
     local spam = true
-    while spam == true do
-        wait(0.1)
+    while spam do
+        task.wait()
     game.Players:Chat(""..text)
     end
 end)
 
 addcmd({"stop","unspam"}, function()
-    local spam = false
+    spam = false
 end)
 
 addcmd({"fixregen"}, function()
     movep(game:GetService("Workspace").Terrain["_Game"].Admin.Regen,CFrame.new(-7.16500044, 5.42999268, 94.7430038, 0, 0, -1, 0, 1, 0, 1, 0, 0))
 end)
 
-addcmd({"movebaseplate","movebp"}, function()
-    if workspace.Terrain._Game.Workspace:FindFirstChild("Baseplate") then
-        movep(
-            workspace.Terrain._Game.Workspace.Baseplate,
-            CFrame.new(Vector3.new(24175, 1000000, 233)))
-    else
-        print("Baseplate Not Loaded!")
-    end
-end)
-
-addcmd({"fixgrass","fixg"}, function()
-    movep(workspace.Terrain._Game.Workspace["Basic House"].SmoothBlockModel40, CFrame.new(-30.0650005, 1.03000009, 72.2430038, 1, 0, 0, 0, 1, 0, 0, 0, 1))
-end)
 
 addcmd({"fixbp","fixbaseplate"}, function()
     if workspace.Terrain._Game.Workspace:FindFirstChild("Baseplate") then
@@ -1096,6 +1098,13 @@ addcmd({"house"}, function()
     )
 end)
 
+addcmd({"antis"},function()
+chatshit("antikill")
+chatshit("antihat")
+chatshit("antipunish")
+chatshit("antirocket")
+chatshit("nok")
+end)
 
 addcmd({"k2"},function(args)
 local plr = gpfs(args[2])
@@ -1140,6 +1149,17 @@ chat("clone "..plr.Name)
 task.wait(0.3)
 end)
 
+addcmd({"movebp"}, function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-57, 4, -29)
+chatshit("stun me")
+task.wait(0.3)
+task.wait(0.3)
+chatshit("unpunish me")
+task.wait(0.3)
+chatshit("skydive me")
+task.wait(0.3)
+alogs()
+end)
 
 addcmd({"antikick"},function()
     Loops.antikick = true
@@ -1160,6 +1180,10 @@ addcmd({"rej","rj","rejoin"},function()
     game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId,game.Players.LocalPlayer)
 end)
 
+addcmd({"m1"}, function()
+chatshit("music "..hideid.." 9047050075")
+end)
+
 addcmd({"antir","antirocket"}, function()
     _G.antirocket = true
     while _G.antirocket do
@@ -1170,6 +1194,10 @@ addcmd({"antir","antirocket"}, function()
         end
         game:GetService("RunService").Heartbeat:Wait()
     end
+end)
+
+addcmd({"loga"},function()
+chatshit("logs")
 end)
 
 addcmd({"antiabuse"}, function()
@@ -1331,6 +1359,56 @@ addcmd({"hban"}, function(args)
     chatshit("hkick".. v.Name)
 end)
 
+addcmd({"slock"}, function()
+chatshit("respawn me")
+for i = 1,100 do 
+chatshit("hat me "..hideid.."18137588505")
+end
+task.wait(0.3)
+for i = 1,100 do 
+chatshit("hat me "..hideid.."18137588505")
+end
+task.wait(0.3)
+for i = 1,100 do 
+chatshit("hat me "..hideid.."18137588505")
+end
+task.wait(0.3)
+for i = 1,100 do 
+chatshit("hat me "..hideid.."18137588505")
+end
+for i = 1,100 do 
+chatshit("hat me "..hideid.."18137588505")
+end
+task.wait(0.3)
+for i = 1,100 do 
+chatshit("hat me "..hideid.."18137588505")
+end
+task.wait(0.3)
+for i = 1,100 do 
+chatshit("hat me "..hideid.."18137588505")
+end
+task.wait(0.3)
+for i = 1,100 do 
+chatshit("hat me "..hideid.."18137588505")
+end
+task.wait(0.3)
+for i = 1,100 do 
+chatshit("hat me "..hideid.."18137588505")
+end
+task.wait(0.3)
+for i = 1,50 do
+chatshit("tp me me")
+end
+task.wait(0.3)
+chatshit("size me 10")
+task.wait(0.3)
+chatshit("clone me")
+task.wait(0.3)
+chatshit("respawn me")
+task.wait(0.3)
+alogs()
+end)
+
 addcmd({"shutdown","close"}, function(args)
     local reason = args[2]
     chatshit("m/ \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nServer crashed by  cat.lua Reason:\n "..result.."\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
@@ -1339,11 +1417,6 @@ addcmd({"shutdown","close"}, function(args)
     task.wait(.1)
     scrash()
 end)
-
-chatshit("nok")
-chatshit("antikill")
-chatshit("antihat")
-chatshit("antirocket")
 
 notif("Cat.Lua Loaded!", Color3.fromRGB(160, 32, 240), 5)
 
@@ -1373,5 +1446,187 @@ lp.Chatted:Connect(function(msg)
     cmds[check](args)    
 end)
 
+coroutine.wrap(function()
+        local function hasPerm(plr)
+            local PermNBC = 66254
+            local PermBC = 64354
+            local UserID = plr.UserId
+            if string.match(game:HttpGet("https://inventory.roblox.com/v1/users/" .. UserID .. "/items/GamePass/" .. PermNBC), PermNBC)
+                or string.match(game:HttpGet("https://inventory.roblox.com/v1/users/" .. UserID .. "/items/GamePass/" .. PermBC), PermBC) then
+                return true
+            else
+                return false
+            end
+        end
+        local function hasPersons(plr)
+            local PersonNBC = 35748
+            local PersonBC = 37127
+            local UserID = plr.UserId
+            if string.match(game:HttpGet("https://inventory.roblox.com/v1/users/" .. UserID .. "/items/GamePass/" .. PersonNBC), PersonNBC)
+                or string.match(game:HttpGet("https://inventory.roblox.com/v1/users/" .. UserID .. "/items/GamePass/" .. PersonBC), PersonBC) then
+                return true
+            else
+                return false
+            end
+        end
+        local players = game:GetService("Players"):GetPlayers()
+        game.StarterGui:SetCoreGuiEnabled("PlayerList", false)
+        local PlayerList = game:GetService("CoreGui").PlayerList
+        
+        
+        local frame = Instance.new("Frame")
+        frame.Size = UDim2.new(0, 200, 0, (60 + 5) * 7)
+        frame.Position = UDim2.new(1, -220, 0.5, -227)
+        frame.BackgroundTransparency = 1
+        frame.BorderSizePixel = 0
+        frame.Parent = PlayerList
+        local function createBubble(parent)
+            local bubble = Instance.new("Frame")
+            bubble.Size = UDim2.new(1, 0, 0, 60)
+            bubble.BackgroundTransparency = 1
+            local cornerRadius = Instance.new("UICorner")
+            cornerRadius.CornerRadius = UDim.new(0, 4)
+            cornerRadius.Parent = bubble
+            local displayNameLabel = Instance.new("TextLabel")
+            displayNameLabel.Size = UDim2.new(1, -10, 0, 60 * 0.5)
+            displayNameLabel.Position = UDim2.new(0, 5, 0, 1)
+            displayNameLabel.BackgroundTransparency = 1
+            displayNameLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+            displayNameLabel.TextSize = 16
+            displayNameLabel.Font = Enum.Font.SourceSansBold
+            displayNameLabel.Parent = bubble
+            local usernameLabel = Instance.new("TextLabel")
+            usernameLabel.Size = UDim2.new(1, -10, 0, 60 * 0.5)
+            usernameLabel.Position = UDim2.new(0, 5, 0, 60 * 0.3)
+            usernameLabel.BackgroundTransparency = 1
+            usernameLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+            usernameLabel.TextSize = 14
+            usernameLabel.Font = Enum.Font.SourceSans
+            usernameLabel.Parent = bubble
+            local thumbnailImage = Instance.new("ImageLabel")
+            thumbnailImage.Size = UDim2.new(0, 30, 0, 30)
+            thumbnailImage.Position = UDim2.new(0, 5, 0, 60 * 0.5 + 9)
+            thumbnailImage.AnchorPoint = Vector2.new(0, 0.5)
+            thumbnailImage.BackgroundTransparency = 1
+            thumbnailImage.Parent = bubble
+            bubble.Parent = parent
+            return bubble, displayNameLabel, usernameLabel, thumbnailImage
+        end
+        local function updatePlayerList()
+            for _, child in pairs(frame:GetChildren()) do
+                child:Destroy()
+            end
+            local totalHeight = frame.Size.Y.Offset
+            local availableHeight = math.min(totalHeight, (#players * (60 + 5)) - 5)
+            local spacing = availableHeight / #players
+            for i, player in pairs(game.Players:GetPlayers()) do
+                local bubble, displayNameLabel, usernameLabel, thumbnailImage = createBubble(frame)
+                bubble.BackgroundColor3 = Color3.fromRGB(20,20,20)
+                bubble.BackgroundTransparency = .1
+                if player.Name == game.Players.LocalPlayer.Name then
+                    displayNameLabel.Text = player.DisplayName
+                    displayNameLabel.TextColor3 = Color3.fromRGB(75,200,255)
+                    usernameLabel.Text = "@" .. player.Name
+                    usernameLabel.TextColor3 = Color3.fromRGB(100,200,255)
+                else
+                    displayNameLabel.Text = player.DisplayName;
+                    usernameLabel.Text = "@" .. player.Name;
+                end
+                local yOffset = (i - 1) * (60 + 5) + -240
+                bubble.Position = UDim2.new(0, 15, 0, yOffset)
+                local thumbnailType = Enum.ThumbnailType.HeadShot
+                local thumbnailSize = Enum.ThumbnailSize.Size420x420
+                local thumbnailUrl = game:GetService("Players"):GetUserThumbnailAsync(player.UserId, thumbnailType, thumbnailSize)
+                thumbnailImage.Image = thumbnailUrl
+                thumbnailImage.Size = UDim2.new(0, 30, 0, 30)
+                local aspectRatio = thumbnailImage.ImageRectSize.X / thumbnailImage.ImageRectSize.Y
+                if thumbnailImage.Size.X.Offset > 30 or thumbnailImage.Size.Y.Offset > 30 then
+                    if aspectRatio > 1 then
+                        thumbnailImage.Size = UDim2.new(0, 30, 0, 30 / aspectRatio)
+                    else
+                        thumbnailImage.Size = UDim2.new(0, 30 * aspectRatio, 0, 30)
+                    end
+                end
+                if hasPerm(player) then
+                    local permissionLabel = Instance.new("TextLabel")
+                    permissionLabel.Size = UDim2.new(0, 80, 0, 30)
+                    permissionLabel.Position = UDim2.new(1, -130, 0, 31)
+                    permissionLabel.BackgroundTransparency = 1
+                    permissionLabel.Text = "Perm"
+                    permissionLabel.TextColor3 = Color3.fromRGB(200, 50, 33)
+                    permissionLabel.TextSize = 14
+                    permissionLabel.Font = Enum.Font.SourceSans
+                    permissionLabel.TextXAlignment = Enum.TextXAlignment.Right
+                    permissionLabel.TextYAlignment = Enum.TextYAlignment.Center
+                    permissionLabel.Parent = bubble
+                end
+                if hasPersons(player) then
+                    local permissionLabelr = Instance.new("TextLabel")
+                    permissionLabelr.Size = UDim2.new(0, 80, 0, 30)
+                    permissionLabelr.Position = UDim2.new(1, -178, 0, 31)
+                    permissionLabelr.BackgroundTransparency = 1
+                    permissionLabelr.Text = "Person299"
+                    permissionLabelr.TextColor3 = Color3.fromRGB(40, 151, 126)
+                    permissionLabelr.TextSize = 14
+                    permissionLabelr.Font = Enum.Font.SourceSans
+                    permissionLabelr.TextXAlignment = Enum.TextXAlignment.Right
+                    permissionLabelr.TextYAlignment = Enum.TextYAlignment.Center
+                    permissionLabelr.Parent = bubble
+                end
+            end
+        end
+        
+        local function slidePlayerList()
+            isSlided = not isSlided
+            local slidePosition = UDim2.new(1, -220, 0.5, -((60 + 5) * 7) / 2)
+            local slideOutPosition = UDim2.new(1, 0, 0.5, -((60 + 5) * 7) / 2)
+            local targetPosition = isSlided and slideOutPosition or slidePosition
+            frame:TweenPosition(targetPosition, Enum.EasingDirection.InOut, Enum.EasingStyle.Quad, 0.3, true)
+        end
+        
+        game:GetService("UserInputService").InputBegan:Connect(function(input)
+            if input.KeyCode == Enum.KeyCode.Tab then
+                slidePlayerList()
+            end
+        end)
+        frame.Position = isSlided and UDim2.new(1, 0, 0.5, -((60 + 5) * 7) / 2) or UDim2.new(1, -220, 0.5, -((60 + 5) * 7) / 2)
+        game:GetService("Players").PlayerAdded:Connect(updatePlayerList)
+        local function isLocalPlayerLeaving(player)
+            if player == game.Players.LocalPlayer then
+                return true
+            else
+                return false
+            end
+        end
+        
+        local Connections = {}
+        local function isLocalPlayerLeaving(player)
+            if player == game.Players.LocalPlayer then
+                return true
+            else
+                return false
+            end
+        end
+        local function removeFunction()
+            for _, Connection in pairs(Connections) do
+                Connection:Disconnect()
+                break
+            end
+        end
+        Connections[#Connections + 1] = game:GetService("Players").PlayerRemoving:Connect(function(player)
+            if isLocalPlayerLeaving(player) then
+                removeFunction()
+                return
+            end
+            updatePlayerList()
+        end)
+        updatePlayerList()
+end)()
 
+--//Auto Startups
+for _,v in pairs(onstart) do
+chatshit(""..v)
+end
+
+-----
 

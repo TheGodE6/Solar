7beb5461f39d33a2751c735404dabbb51bd0cce3
TheGodE6 <110555637+TheGodE6@users.noreply.github.com>
2024-10-19 09:32:54 -0700

    Update Venus.lua

--- Venus.lua
+++ Venus.lua
@@ -6,13 +6,13 @@ local function chat(msg)
     game.Players:Chat(msg)
 end
 local function targets(plr)
-local args = {}
+local plrTargets = {}
         for i, plr in pairs(game.Players:GetPlayers()) do
         if string.sub(plr.Name:lower(),1,#plr) == plr:lower() then
-            table.insert(args, plr)
+            table.insert(plrTargets, plr)
 end
       end
-                return args
+                return plrTargets
 end
 
 lp.Chatted:Connect(function(msg)

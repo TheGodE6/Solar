49a41f15a7c21f1e592cf41444bd5e25e3860482
TheGodE6 <110555637+TheGodE6@users.noreply.github.com>
2024-10-19 09:36:29 -0700

    Update Venus.lua

--- Venus.lua
+++ Venus.lua
@@ -5,14 +5,28 @@ local prefix = ">"
 local function chat(msg)
     game.Players:Chat(msg)
 end
-local function targets(plr)
-local plrTargets = {}
-        for i, plr in pairs(game.Players:GetPlayers()) do
-        if string.sub(plr.Name:lower(),1,#plr) == plr:lower() then
-            table.insert(plrTargets, plr)
-end
-      end
-                return plrTargets
+function GetPlayer(target)
+    local plrs = game:service("Players"):GetPlayers()
+    if target:lower() == "all" then
+        return plrs
+    elseif target:lower() == "others" then
+        for index, plr in pairs(plrs) do
+            if plr == game:service("Players").LocalPlayer then
+                table.remove(plrs, index)
+                return plrs
+            end
+        end
+    elseif target:lower() == "me" then
+        return {game:service("Players").LocalPlayer}
+    else
+        local plrTargets = {}
+        for index, plr in pairs(plrs) do
+            if plr.Name:sub(1, #target):lower() == target:lower() or plr.DisplayName:sub(1, #target):lower() == target:lower()  then
+                table.insert(plrTargets, plr)
+            end
+        end
+        return plrTargets
+    end
 end
 
 lp.Chatted:Connect(function(msg)
@@ -20,7 +34,7 @@ local args = msg:split(" ")
 
 
     if msg == prefix.."test" then
-                for i, plr in pairs(targets(args[2]) do
+                for i, plr in pairs(GetPlayer(args[2]) do
             chat("h "..plr.Name.." test")
 end
 end
